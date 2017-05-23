f818c821ee6995c55226a0d1672f3dc670bf7972
steve dekerf <steve.dekerf@hotmail.com>
2017-05-23 22:30:27 +0200

    register routing

---
+++ dekerf_steve_Project/Controllers/RegisterController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace dekerf_steve_Project.Controllers
+{
+    public class RegisterController : Controller
+    {
+        // GET: Register
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Test()
+        {
+
+
+
+
+
+
+
+
+
+
+            return View();
+        }
+
+    }
+}
